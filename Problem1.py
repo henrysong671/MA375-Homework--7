0ffd689df86b872abf1690426e5cda34c6c6a364
Henry Song <henry.song671@gmail.com>
2021-05-04 17:53:01 +1000

    formatting and comments

--- Problem1.py
+++ Problem1.py
@@ -20,27 +20,30 @@ def part_a():
     plt.show()
 
 # Performs manual calculation of error function
-# 0-0.1, 0-0.2, 0-0.3 ... 0-1
+
 def part_b():
     x = np.linspace(0.1, 1, 10) # creates a range of x values
-    integral = lambda t: ((np.e)**(-t**2))  # only the integral part of the erf function
-    f = (2/np.sqrt(np.pi))
+    integral = lambda t: ((np.e)**(-t**2))  # integral part of the erf function
+    f = (2/np.sqrt(np.pi))  #first half (non-integral part) of erf function
 
     results = []
+    
+    # loop first calculates the value of integral using scipy.integrate.quad(), then adds overall result to array after calculation
     for i in x:
         integrated = integrate.quad(integral, 0, i) #calculates integral part of function
-        results.append(f*integrated[0])    # multiplies integral part by 2/sqrt(pi), then adds result to array
+        results.append(f*integrated[0])    # multiplies integral part by non-integral part, then adds result to array
     
     print("x-value   Results")
-    for i in range(len(x)): print("%0.1f" % x[i], "     ", "%0.8f" % results[i])
+    for i in range(len(x)): print("%0.1f" % x[i], "     ", "%0.12f" % results[i])
     return results
 
 # Performs error function calculations using built in erf() method
 def part_c(results):
     x = np.linspace(0.1, 1, 10) # creates a range of x values
     y = special.erf(x)  # calculates error function values using erf()
-    print("x-value   Part C Results   Part B Results")
-    for i in range(len(x)): print("%0.1f" % x[i], "     ", "%0.8f" % y[i], "     ", "%0.8f" % results[i])  #prints erf value for each x value
+    print("x-value  |  Part C Results  |  Part B Results")
+    print("---------------------------------------------")
+    for i in range(len(x)): print("%0.1f" % x[i], "     | ", "%0.12f" % y[i], " | ", "%0.12f" % results[i])  #prints erf value for each x value
 
 
 if __name__ == "__main__":
@@ -48,11 +51,11 @@ if __name__ == "__main__":
     part_a()
     print()
 
-    print("Part B:")
-    print("NOTE: values are rounded to 8 decimal places")
+    print("Part B: Manual erf function calculations")
+    print("NOTE: values are rounded to 12 decimal places")
     part_b_results = part_b()
     print()
 
-    print("Part C: ")
-    print("NOTE: values are rounded to 8 decimal places")
+    print("Part C: SciPy erf function compared with manual calculations")
+    print("NOTE: values are rounded to 12 decimal places")
     part_c(part_b_results)
