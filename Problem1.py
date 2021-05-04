2107118bdfc83930cce0dac92e35f6da02af4d20
Henry Song <henry.song671@gmail.com>
2021-05-04 17:45:33 +1000

    finished part b

--- Problem1.py
+++ Problem1.py
@@ -20,32 +20,39 @@ def part_a():
     plt.show()
 
 # Performs manual calculation of error function
+# 0-0.1, 0-0.2, 0-0.3 ... 0-1
 def part_b():
-    # x = np.linspace(0.1, 1, 10) # creates a range of x values
-    # func = lambda t: (np.e)**(-t**2)
+    x = np.linspace(0.1, 1, 10) # creates a range of x values
+    integral = lambda t: ((np.e)**(-t**2))  # only the integral part of the erf function
+    f = (2/np.sqrt(np.pi))
 
-
-    # f = (2/np.sqrt(np.pi))
-    # print(f)
-    return 1
+    results = []
+    for i in x:
+        integrated = integrate.quad(integral, 0, i) #calculates integral part of function
+        results.append(f*integrated[0])    # multiplies integral part by 2/sqrt(pi), then adds result to array
+    
+    print("x-value   Results")
+    for i in range(len(x)): print("%0.1f" % x[i], "     ", "%0.8f" % results[i])
+    return results
 
 # Performs error function calculations using built in erf() method
-def part_c():
+def part_c(results):
     x = np.linspace(0.1, 1, 10) # creates a range of x values
     y = special.erf(x)  # calculates error function values using erf()
     print("x-value   Part C Results   Part B Results")
-    for i in range(len(x)): print("%0.1f" % x[i], "     ", "%0.5f" % y[i], "        ", "%0.5f" % y[i])  #prints erf value for each x value
+    for i in range(len(x)): print("%0.1f" % x[i], "     ", "%0.8f" % y[i], "     ", "%0.8f" % results[i])  #prints erf value for each x value
 
 
 if __name__ == "__main__":
-    # print("Part A: Please see separate plot window")
-    # part_a()
-    # print()
+    print("Part A: Please see separate plot window")
+    part_a()
+    print()
 
     print("Part B:")
-    part_b()
+    print("NOTE: values are rounded to 8 decimal places")
+    part_b_results = part_b()
     print()
 
     print("Part C: ")
-    print("*values are rounded to 5 decimal places")
-    part_c()
+    print("NOTE: values are rounded to 8 decimal places")
+    part_c(part_b_results)
